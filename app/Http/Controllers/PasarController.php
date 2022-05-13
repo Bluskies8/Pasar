215f68047ae64c3437e84239bcd19424eee7fd77
Bluskies8 <53179442+Bluskies8@users.noreply.github.com>
2022-05-13 14:05:26 +0700

    Initial commit

---
+++ app/Http/Controllers/PasarController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pasar;
+use Illuminate\Http\Request;
+
+class PasarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\pasar  $pasar
+     * @return \Illuminate\Http\Response
+     */
+    public function show(pasar $pasar)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\pasar  $pasar
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(pasar $pasar)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\pasar  $pasar
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, pasar $pasar)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\pasar  $pasar
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(pasar $pasar)
+    {
+        //
+    }
+}
