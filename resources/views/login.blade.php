a1921aaa3d0603e732cb1dcf569fdb91a1d4825f
Mitchell Arthur <mitchell2@mhs.stts.edu>
2022-05-16 15:12:23 +0700

    Login & Register

---
+++ resources/views/login.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>pasar</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body class="d-flex justify-content-center align-items-center" style="background: #f1f7fc;height: 90vh;">
+    <section class="login-clean container">
+        <form method="post">
+            <h2 class="visually-hidden">Login Form</h2>
+            <div class="illustration"><img class="w-100" src="assets/img/logo.svg"></div>
+            <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Masukan E-mail"></div>
+            <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Masukan Password"></div>
+            <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit" style="background: var(--bs-info);">Log In</button></div><a class="forgot" href="#">Lupa e-mail atau password?</a>
+        </form>
+    </section>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
---
+++ resources/views/register.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>pasar</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body class="d-flex justify-content-center align-items-center" style="background: #f1f7fc;height: 90vh;">
+    <section class="register-photo container">
+        <div class="form-container">
+            <div class="image-holder"></div>
+            <form method="post">
+                <h2 class="text-center"><strong>Buat</strong> akun baru.</h2>
+                <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Masukan E-mail"></div>
+                <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Masukan Password"></div>
+                <div class="mb-3"><input class="form-control" type="password" name="password-repeat" placeholder="Masukan Ulang Password"></div>
+                <div class="mb-3">
+                    <div class="form-check"><label class="form-check-label"><input class="form-check-input my-3" type="checkbox">Saya setuju dengan syarat dan ketentuan yang berlaku.</label></div>
+                </div>
+                <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit" style="background: var(--bs-info);">Sign Up</button></div><a class="already" href="#">Sudah punya akun? Login disini.</a>
+            </form>
+        </div>
+    </section>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
