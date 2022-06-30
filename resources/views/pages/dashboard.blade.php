32df38c6a3eb35db36bf1f6a09f9fa7e35a33403
Mitchell Arthur <64058372+Mitchell-Arthur@users.noreply.github.com>
2022-06-30 15:40:27 +0700

    Update dashboard.blade.php
    Add card laporan biaya kuli

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -8,30 +8,36 @@
         <li role="presentation" class="nav-item mx-3" style="height: initial"><a role="tab" data-bs-toggle="tab" class="nav-link px-0" href="#tab-2">Logs</a></li>
     </ul>
     <div class="tab-content" style="height: calc(100% - 50px);">
-        <div role="tabpanel" class="tab-pane active py-5" id="tab-1">
-            <div id="laporan-biaya-kuli" class="row">
-                <div class="col-3 d-flex justify-content-center align-items-center">
-                    <div class="d-flex flex-column justify-content-center p-3 text-center" style="height: 200px;width: 200px;border-radius: 50%;display: inline-block;background-color: rgba(255, 159, 64, 0.2); border: 1px solid rgba(255, 159, 64, 1);">
-                        <p style="font-weight: 600;">Sedang terkumpul bulan ini</p>
-                        <h2>Rp <span class="thousand-separator">800.000</span></h2>
-                    </div>
-                </div>
-                <div class="col-3 d-flex justify-content-center align-items-center">
-                    <div class="d-flex flex-column justify-content-center p-3 text-center" style="height: 200px;width: 200px;border-radius: 50%;display: inline-block;background-color: rgba(153, 102, 255, 0.2); border: 1px solid rgba(153, 102, 255, 1);">
-                        <p style="font-size: 32px;font-weight: 600;">Mei</p>
-                        <h2>Rp <span class="thousand-separator">800.000</span></h2>
-                    </div>
-                </div>
-                <div class="col-3 d-flex justify-content-center align-items-center">
-                    <div class="d-flex flex-column justify-content-center p-3 text-center" style="height: 200px;width: 200px;border-radius: 50%;display: inline-block;background-color: rgba(75, 192, 192, 0.2); border: 1px solid rgba(75, 192, 192, 1);">
-                        <p style="font-size: 32px;font-weight: 600;">April</p>
-                        <h2>Rp <span class="thousand-separator">800.000</span></h2>
-                    </div>
-                </div>
-                <div class="col-3 d-flex justify-content-center align-items-center">
-                    <div class="d-flex flex-column justify-content-center p-3 text-center" style="height: 200px;width: 200px;border-radius: 50%;display: inline-block;background-color: rgba(255, 206, 86, 0.2); border: 1px solid rgba(255, 206, 86, 1);">
-                        <p style="font-size: 32px;font-weight: 600;">Maret</p>
-                        <h2>Rp <span class="thousand-separator">800.000</span></h2>
+        <div role="tabpanel" class="tab-pane active py-5 px-4" id="tab-1">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="card-title" style="color: rgb(24, 144, 255);">Laporan Biaya Kuli</h2>
+                    <hr />
+                    <div id="laporan-biaya-kuli" class="row">
+                        <div class="col-3 d-flex justify-content-center align-items-center">
+                            <div class="d-flex flex-column justify-content-center p-3 text-center" style="height: 200px;width: 200px;border-radius: 50%;display: inline-block;background-color: rgba(255, 159, 64, 0.2); border: 1px solid rgba(255, 159, 64, 1);">
+                                <p style="font-weight: 600;">Sedang terkumpul bulan ini</p>
+                                <h2>Rp <span class="thousand-separator">800.000</span></h2>
+                            </div>
+                        </div>
+                        <div class="col-3 d-flex justify-content-center align-items-center">
+                            <div class="d-flex flex-column justify-content-center p-3 text-center" style="height: 200px;width: 200px;border-radius: 50%;display: inline-block;background-color: rgba(153, 102, 255, 0.2); border: 1px solid rgba(153, 102, 255, 1);">
+                                <p style="font-size: 32px;font-weight: 600;">Mei</p>
+                                <h2>Rp <span class="thousand-separator">800.000</span></h2>
+                            </div>
+                        </div>
+                        <div class="col-3 d-flex justify-content-center align-items-center">
+                            <div class="d-flex flex-column justify-content-center p-3 text-center" style="height: 200px;width: 200px;border-radius: 50%;display: inline-block;background-color: rgba(75, 192, 192, 0.2); border: 1px solid rgba(75, 192, 192, 1);">
+                                <p style="font-size: 32px;font-weight: 600;">April</p>
+                                <h2>Rp <span class="thousand-separator">800.000</span></h2>
+                            </div>
+                        </div>
+                        <div class="col-3 d-flex justify-content-center align-items-center">
+                            <div class="d-flex flex-column justify-content-center p-3 text-center" style="height: 200px;width: 200px;border-radius: 50%;display: inline-block;background-color: rgba(255, 206, 86, 0.2); border: 1px solid rgba(255, 206, 86, 1);">
+                                <p style="font-size: 32px;font-weight: 600;">Maret</p>
+                                <h2>Rp <span class="thousand-separator">800.000</span></h2>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -44,6 +50,7 @@
                 </div>
             </div>
         </div>
+
         <div role="tabpanel" class="tab-pane" id="tab-2">
             <p>Content for tab 2.</p>
         </div>
