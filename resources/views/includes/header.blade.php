b6b935597ba0c7975f77b1474c287c35435bec65
Mitchell Arthur <64058372+Mitchell-Arthur@users.noreply.github.com>
2022-06-30 17:37:34 +0700

    Update header.blade.php
    Hide header buttons

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,32 +3,36 @@
         <p style="color: rgb(24, 144, 255);font-size: 30px;font-weight: bold;">LOGO</p>
     </div>
     <div class="w-100 d-flex justify-content-md-between justify-content-end">
-        <div class="d-md-flex align-items-center d-none">
-            <i class="fas fa-plus-circle fa-xl ms-3 me-2" data-bs-toggle="tooltip" data-bss-tooltip="" data-bs-placement="bottom" id="quick-create" title="Quick Create"></i>
-            <i class="fas fa-clock fa-xl ms-2 me-3" data-bs-toggle="tooltip" data-bss-tooltip="" data-bs-placement="bottom" id="recent-activity" title="Recent Activity"></i>
-            <div id="searchbar" class="d-none d-lg-flex">
-                <div class="dropdown">
-                    <button class="btn dropdown-toggle d-flex align-items-center" aria-expanded="false" data-bs-toggle="dropdown" type="button" style="height: 34px;box-shadow: none;">
-                        <i class="fas fa-search" style="font-size: 16px;"></i></button>
-                    <div class="dropdown-menu">
-                        <a class="dropdown-item" href="#">First Item</a>
-                        <a class="dropdown-item" href="#">Second Item</a>
-                        <a class="dropdown-item" href="#">Third Item</a>
-                    </div>
-                </div><input type="text" style="outline: none;border: none;width: 225px;" placeholder="Search">
+        <!--
+            <div class="d-md-flex align-items-center d-none">
+                <i class="fas fa-plus-circle fa-xl ms-3 me-2" data-bs-toggle="tooltip" data-bss-tooltip="" data-bs-placement="bottom" id="quick-create" title="Quick Create"></i>
+                <i class="fas fa-clock fa-xl ms-2 me-3" data-bs-toggle="tooltip" data-bss-tooltip="" data-bs-placement="bottom" id="recent-activity" title="Recent Activity"></i>
+                <div id="searchbar" class="d-none d-lg-flex">
+                    <div class="dropdown">
+                        <button class="btn dropdown-toggle d-flex align-items-center" aria-expanded="false" data-bs-toggle="dropdown" type="button" style="height: 34px;box-shadow: none;">
+                            <i class="fas fa-search" style="font-size: 16px;"></i></button>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">First Item</a>
+                            <a class="dropdown-item" href="#">Second Item</a>
+                            <a class="dropdown-item" href="#">Third Item</a>
+                        </div>
+                    </div><input type="text" style="outline: none;border: none;width: 225px;" placeholder="Search">
+                </div>
             </div>
-        </div>
-        <div class="d-flex align-items-center">
-            <div class="me-3 position-relative d-md-block d-none">
-                <i class="fas fa-bell fa-xl" data-bs-toggle="tooltip" data-bss-tooltip="" data-bs-placement="bottom" title="Notifications"></i>
-                <p class="position-absolute rounded-circle d-flex align-items-center justify-content-center notification-nudge">0</p>
-            </div>
-            <div class="me-3 position-relative d-md-block d-none">
-                <i class="fas fa-folder-open fa-xl"></i>
-                <p class="position-absolute rounded-circle d-flex align-items-center justify-content-center notification-nudge">0</p>
-            </div>
-            <i class="fas fa-cog fa-xl me-3"></i>
-            <i class="fas fa-question-circle fa-xl me-3"></i>
+        -->
+        <div class="d-flex align-items-center justify-content-end w-100">
+            <!--
+                <div class="me-3 position-relative d-md-block d-none">
+                    <i class="fas fa-bell fa-xl" data-bs-toggle="tooltip" data-bss-tooltip="" data-bs-placement="bottom" title="Notifications"></i>
+                    <p class="position-absolute rounded-circle d-flex align-items-center justify-content-center notification-nudge">0</p>
+                </div>
+                <div class="me-3 position-relative d-md-block d-none">
+                    <i class="fas fa-folder-open fa-xl"></i>
+                    <p class="position-absolute rounded-circle d-flex align-items-center justify-content-center notification-nudge">0</p>
+                </div>
+                <i class="fas fa-cog fa-xl me-3"></i>
+                <i class="fas fa-question-circle fa-xl me-3"></i>
+            -->
             <div class="dropdown me-4">
                 <button class="btn p-0" aria-expanded="false" data-bs-toggle="dropdown" type="button" style="outline: none;box-shadow: none;">
                     <img class="rounded-circle" style="width: 40px;height: 40px;" src="{{asset('img/logo.svg')}}">
