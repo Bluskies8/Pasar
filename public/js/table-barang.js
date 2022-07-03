abe61c978e1dd0c27fdc0ff43642ff52051ce605
Mitchell Arthur <mitchell2@mhs.stts.edu>
2022-07-03 17:33:57 +0700

    remove inspect

--- public/js/table-barang.js
+++ public/js/table-barang.js
@@ -2,11 +2,13 @@ $(document).ready(function(){
     document.addEventListener('contextmenu', function(e) {
         e.preventDefault();
     });
+
     $(document).keydown(function(e){
         if(e.which === 123){
             return false;
         }
     });
+
     $('#table-barang').DataTable({
         paging: false,
         info: false,
--- public/js/table-invoice.js
+++ public/js/table-invoice.js
@@ -1,4 +1,14 @@
 $(document).ready(function() {
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
+
+    $(document).keydown(function(e){
+        if(e.which === 123){
+            return false;
+        }
+    });
+
     $('#table-invoice').DataTable({
         paging: false,
         columns: [
