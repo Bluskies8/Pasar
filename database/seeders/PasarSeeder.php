104c0af4968c00e0b3a4e41a46ed22aeeff21031
Bluskies8 <53179442+Bluskies8@users.noreply.github.com>
2022-07-23 20:04:03 +0700

    Update PasarSeeder.php

--- database/seeders/PasarSeeder.php
+++ database/seeders/PasarSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\pasar;
+use App\Models\shif;
 
 class PasarSeeder extends Seeder
 {
@@ -18,6 +19,24 @@ class PasarSeeder extends Seeder
         pasar::create([
             'nama'=> "pasar 47",
             'alamat'=>"JL. tanjungsari 47"
+        ]);
+        shif::create([
+            'pasar_id' => '1',
+            'number' => 1,
+            'start' => '2022-05-30 08:00:00',
+            'end' => '2022-05-30 15:00:00',
+        ]);
+        shif::create([
+            'pasar_id' => '1',
+            'number' => 2,
+            'start' => '2022-05-30 15:00:00',
+            'end' => '2022-05-30 22:00:00',
+        ]);
+        shif::create([
+            'pasar_id' => '1',
+            'number' => 3,
+            'start' => '2022-05-30 22:00:00',
+            'end' => '2022-05-31 08:00:00',
         ]);
     }
 }
