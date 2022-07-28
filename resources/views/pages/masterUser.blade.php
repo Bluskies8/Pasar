3603ab0032b668ebc90c76b96b1f711509c0e136
Mitchell Arthur <mitchell2@mhs.stts.edu>
2022-07-28 16:30:19 +0700

    revisi master user input shift

--- resources/views/pages/masterUser.blade.php
+++ resources/views/pages/masterUser.blade.php
@@ -5,6 +5,13 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
+<style>
+    .dropdown-shif p:hover {
+        background-color: rgb(33,37,41,0.8);
+        color: white;
+    }
+</style>
+
 <header id="content-header" class="d-flex align-items-center justify-content-between" style="height: 50px;">
     <div class="dropdown h-100">
         <button class="btn dropdown-toggle h-100" aria-expanded="false" data-bs-toggle="dropdown" type="button" style="visibility: hidden;">All Item</button>
@@ -88,13 +95,199 @@
                     <div class="form-check"><input type="radio" class="form-check-input input-radio" id="role-4" name="role" value="4"/><label class="form-check-label" for="role-4">Checker</label></div>
                 </div>
                 <div class="d-flex justify-content-between">
-                    <div class="position-relative"><input type="datetime-local" class="form-control" id="input-shif-masuk" style="width: 200px;" />
+                    <div class="form-control position-relative d-flex flex-column" id="container-shif-masuk" style="width: 200px;height: 34px;">
+                        <p class="d-flex justify-content-around" style="padding: 0 14px;"><span id="jam-masuk">00</span> : <span id="menit-masuk">00</span></p>
+                        <div class="position-absolute d-none d-flex w-100 dropdown-shif" id="dropdown-shif-masuk" style="top: 32px;left: 0px;height: 100px;cursor: default;">
+                            <div class="form-control px-0" id="dropdown-jam-masuk" style="overflow-y: auto;">
+                                <p class="text-center">00</p>
+                                <p class="text-center">01</p>
+                                <p class="text-center">02</p>
+                                <p class="text-center">03</p>
+                                <p class="text-center">04</p>
+                                <p class="text-center">05</p>
+                                <p class="text-center">06</p>
+                                <p class="text-center">07</p>
+                                <p class="text-center">08</p>
+                                <p class="text-center">09</p>
+                                <p class="text-center">10</p>
+                                <p class="text-center">11</p>
+                                <p class="text-center">12</p>
+                                <p class="text-center">13</p>
+                                <p class="text-center">14</p>
+                                <p class="text-center">15</p>
+                                <p class="text-center">16</p>
+                                <p class="text-center">17</p>
+                                <p class="text-center">18</p>
+                                <p class="text-center">19</p>
+                                <p class="text-center">20</p>
+                                <p class="text-center">21</p>
+                                <p class="text-center">22</p>
+                                <p class="text-center">23</p>
+                            </div>
+                            <div class="form-control px-0" id="dropdown-menit-masuk" style="overflow-y: auto;">
+                                <p class="text-center">00</p>
+                                <p class="text-center">01</p>
+                                <p class="text-center">02</p>
+                                <p class="text-center">03</p>
+                                <p class="text-center">04</p>
+                                <p class="text-center">05</p>
+                                <p class="text-center">06</p>
+                                <p class="text-center">07</p>
+                                <p class="text-center">08</p>
+                                <p class="text-center">09</p>
+                                <p class="text-center">10</p>
+                                <p class="text-center">11</p>
+                                <p class="text-center">12</p>
+                                <p class="text-center">13</p>
+                                <p class="text-center">14</p>
+                                <p class="text-center">15</p>
+                                <p class="text-center">16</p>
+                                <p class="text-center">17</p>
+                                <p class="text-center">18</p>
+                                <p class="text-center">19</p>
+                                <p class="text-center">20</p>
+                                <p class="text-center">21</p>
+                                <p class="text-center">22</p>
+                                <p class="text-center">23</p>
+                                <p class="text-center">24</p>
+                                <p class="text-center">25</p>
+                                <p class="text-center">26</p>
+                                <p class="text-center">27</p>
+                                <p class="text-center">28</p>
+                                <p class="text-center">29</p>
+                                <p class="text-center">30</p>
+                                <p class="text-center">31</p>
+                                <p class="text-center">32</p>
+                                <p class="text-center">33</p>
+                                <p class="text-center">34</p>
+                                <p class="text-center">35</p>
+                                <p class="text-center">36</p>
+                                <p class="text-center">37</p>
+                                <p class="text-center">38</p>
+                                <p class="text-center">39</p>
+                                <p class="text-center">40</p>
+                                <p class="text-center">41</p>
+                                <p class="text-center">42</p>
+                                <p class="text-center">43</p>
+                                <p class="text-center">44</p>
+                                <p class="text-center">45</p>
+                                <p class="text-center">46</p>
+                                <p class="text-center">47</p>
+                                <p class="text-center">48</p>
+                                <p class="text-center">49</p>
+                                <p class="text-center">50</p>
+                                <p class="text-center">51</p>
+                                <p class="text-center">52</p>
+                                <p class="text-center">53</p>
+                                <p class="text-center">54</p>
+                                <p class="text-center">55</p>
+                                <p class="text-center">56</p>
+                                <p class="text-center">57</p>
+                                <p class="text-center">58</p>
+                                <p class="text-center">59</p>
+                            </div>
+                            <button class="btn btn-success btn-sm position-absolute w-100" type="button" style="left: 0px;top: 100px;">Tutup</button>
+                        </div>
+                        <input type="hidden" id="input-shif-masuk"/>
                         <p class="position-absolute px-1" style="top: -12px;left: 10px;font-size: 14px;background-color: white;">Shift Mulai</p>
-                        <p class="small text-danger error-msg-masuk" id = "error-msg-masuk"></p>
+                        <p class="small text-danger error-msg-masuk" id="error-msg-masuk"></p>
                     </div>
-                    <div class="position-relative"><input type="datetime-local" class="form-control" id="input-shif-keluar" style="width: 200px;" />
+                    <div class="form-control position-relative d-flex flex-column" id="container-shif-keluar" style="width: 200px;height: 34px;">
+                        <p class="d-flex justify-content-around" style="padding: 0 14px;"><span id="jam-keluar">00</span> : <span id="menit-keluar">00</span></p>
+                        <div class="position-absolute d-none d-flex w-100 dropdown-shif" id="dropdown-shif-keluar" style="top: 32px;left: 0px;height: 100px;cursor: default;">
+                            <div class="form-control px-0" id="dropdown-jam-keluar" style="overflow-y: auto;">
+                                <p class="text-center">00</p>
+                                <p class="text-center">01</p>
+                                <p class="text-center">02</p>
+                                <p class="text-center">03</p>
+                                <p class="text-center">04</p>
+                                <p class="text-center">05</p>
+                                <p class="text-center">06</p>
+                                <p class="text-center">07</p>
+                                <p class="text-center">08</p>
+                                <p class="text-center">09</p>
+                                <p class="text-center">10</p>
+                                <p class="text-center">11</p>
+                                <p class="text-center">12</p>
+                                <p class="text-center">13</p>
+                                <p class="text-center">14</p>
+                                <p class="text-center">15</p>
+                                <p class="text-center">16</p>
+                                <p class="text-center">17</p>
+                                <p class="text-center">18</p>
+                                <p class="text-center">19</p>
+                                <p class="text-center">20</p>
+                                <p class="text-center">21</p>
+                                <p class="text-center">22</p>
+                                <p class="text-center">23</p>
+                            </div>
+                            <div class="form-control px-0" id="dropdown-menit-keluar" style="overflow-y: auto;">
+                                <p class="text-center">00</p>
+                                <p class="text-center">01</p>
+                                <p class="text-center">02</p>
+                                <p class="text-center">03</p>
+                                <p class="text-center">04</p>
+                                <p class="text-center">05</p>
+                                <p class="text-center">06</p>
+                                <p class="text-center">07</p>
+                                <p class="text-center">08</p>
+                                <p class="text-center">09</p>
+                                <p class="text-center">10</p>
+                                <p class="text-center">11</p>
+                                <p class="text-center">12</p>
+                                <p class="text-center">13</p>
+                                <p class="text-center">14</p>
+                                <p class="text-center">15</p>
+                                <p class="text-center">16</p>
+                                <p class="text-center">17</p>
+                                <p class="text-center">18</p>
+                                <p class="text-center">19</p>
+                                <p class="text-center">20</p>
+                                <p class="text-center">21</p>
+                                <p class="text-center">22</p>
+                                <p class="text-center">23</p>
+                                <p class="text-center">24</p>
+                                <p class="text-center">25</p>
+                                <p class="text-center">26</p>
+                                <p class="text-center">27</p>
+                                <p class="text-center">28</p>
+                                <p class="text-center">29</p>
+                                <p class="text-center">30</p>
+                                <p class="text-center">31</p>
+                                <p class="text-center">32</p>
+                                <p class="text-center">33</p>
+                                <p class="text-center">34</p>
+                                <p class="text-center">35</p>
+                                <p class="text-center">36</p>
+                                <p class="text-center">37</p>
+                                <p class="text-center">38</p>
+                                <p class="text-center">39</p>
+                                <p class="text-center">40</p>
+                                <p class="text-center">41</p>
+                                <p class="text-center">42</p>
+                                <p class="text-center">43</p>
+                                <p class="text-center">44</p>
+                                <p class="text-center">45</p>
+                                <p class="text-center">46</p>
+                                <p class="text-center">47</p>
+                                <p class="text-center">48</p>
+                                <p class="text-center">49</p>
+                                <p class="text-center">50</p>
+                                <p class="text-center">51</p>
+                                <p class="text-center">52</p>
+                                <p class="text-center">53</p>
+                                <p class="text-center">54</p>
+                                <p class="text-center">55</p>
+                                <p class="text-center">56</p>
+                                <p class="text-center">57</p>
+                                <p class="text-center">58</p>
+                                <p class="text-center">59</p>
+                            </div>
+                            <button class="btn btn-success btn-sm position-absolute w-100" type="button" style="left: 0px;top: 100px;">Tutup</button>
+                        </div>
+                        <input type="hidden" id="input-shif-keluar"/>
                         <p class="position-absolute px-1" style="top: -12px;left: 10px;font-size: 14px;background-color: white;">Shift Selesai</p>
-                        <p class="small text-danger error-msg-keluar" id = "error-msg-keluar"></p>
+                        <p class="small text-danger error-msg-keluar" id="error-msg-keluar"></p>
                     </div>
                     <button id="btn-reset-clock" class="btn btn-sm" type="button" style="background-color: #38A34A; width: 34px;">
                         <i class="fas fa-trash fa-l" style="color: white"></i>
