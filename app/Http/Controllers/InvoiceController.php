cbda3d079fc71d8203cb1a890ada381ba2c4450a
Bluskies8 <53179442+Bluskies8@users.noreply.github.com>
2022-08-15 10:16:24 +0700

    Update InvoiceController.php

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -66,7 +66,7 @@ class InvoiceController extends Controller
             }
         }
         $listrik = listrik::orderBy('value')->get();
-        
+        dd($parkir);
         return view('pages.invoice',[
             'date'=> $carbon,
             'invoice'=> $data,
