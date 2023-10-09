49a2390e907c7d5ce0dde8e1e653d06996ab5ada
Bluskies8 <53179442+Bluskies8@users.noreply.github.com>
2023-10-09 13:13:35 +0700

    Update HtransController.php

--- app/Http/Controllers/HtransController.php
+++ app/Http/Controllers/HtransController.php
@@ -169,7 +169,7 @@ class HtransController extends Controller
                         ->where('created_at', "like", "{$request->year}-{$request->month}%")
                         // ->paginate(100);
                         ->get();
-                        dd($temp);
+                        // dd($temp);
         }
         return view('components/tableStock', [
             'data' => $temp,
